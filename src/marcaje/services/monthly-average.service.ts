8272324e86fc6ad3f675bdce29da6542fd4fc055
Sharked <jose.carrillo@alumnos.ucn.cl>
2024-06-28 10:39:46 -0400

    Modify imports to * as

--- src/marcaje/services/monthly-average.service.ts
+++ src/marcaje/services/monthly-average.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { MonthlyAverageHours } from 'src/entities/monthly-average-hours';
 import * as shedule from '@nestjs/schedule';
-import * as moment from 'moment';
+import moment from 'moment';
 
 @Injectable()
 export class MonthlyAverageHoursService {
